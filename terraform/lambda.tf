209b47fd626fd036419b676ce7326a8d423f858b
maguzman5 <maguzman5@uc.cl>
2023-08-28 13:09:08 -0400

    lambda to trigger glue

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -21,3 +21,27 @@ resource "aws_lambda_permission" "apigw" {
 
   source_arn = "${aws_api_gateway_rest_api.gcc_api.execution_arn}/*/*"
 }
+
+resource "aws_lambda_function" "trigger_glue_lambda" {
+  function_name    = var.trigger_glue_function_name
+  filename         = data.archive_file.gcc_trigger_glue_zip.output_path
+  source_code_hash = data.archive_file.gcc_trigger_glue_zip.output_base64sha256
+
+  role       = aws_iam_role.gcc_trigger_glue_exec_role.arn
+  handler    = "main.lambda_handler"
+  runtime    = "python3.10"
+  depends_on = [data.archive_file.gcc_trigger_glue_zip]
+
+  tags = {
+    project = "GCC"
+  }
+}
+
+# resource "aws_s3_bucket_notification" "trigger_glue_notification" {
+#   bucket = aws_s3_bucket.raw_bucket.id
+
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.trigger_glue_lambda.arn
+#     events              = ["s3:ObjectCreated:*"]
+#   }
+# }
