0cc4eecde27cce6ae4270aafc1cef02ffdb87a99
maguzman5 <maguzman5@uc.cl>
2023-08-28 01:28:36 -0400

    lambda change name and add  to api gateway

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "lambda_function" {
+resource "aws_lambda_function" "upload_csv_lambda" {
   function_name    = var.upload_csv_function_name
   filename         = data.archive_file.gcc_upload_csv_zip.output_path
   source_code_hash = data.archive_file.gcc_upload_csv_zip.output_base64sha256
@@ -11,4 +11,13 @@ resource "aws_lambda_function" "lambda_function" {
   tags = {
     project = "GCC"
   }
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.upload_csv_lambda.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.gcc_api.execution_arn}/*/*"
 }
