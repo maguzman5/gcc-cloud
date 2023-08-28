22fe70a8b686dc8c4c7782590f818d7b3c511c90
maguzman5 <maguzman5@uc.cl>
2023-08-27 22:36:09 -0400

    Initial terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "gcc-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+  
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
