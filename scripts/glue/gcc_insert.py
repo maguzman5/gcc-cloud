12b8bb465bb3e8cd618f7200460369b102b7fab4
maguzman5 <maguzman5@uc.cl>
2023-08-30 15:10:17 -0400

    extract and rm of nulls and no matchs

--- scripts/glue/gcc_insert.py
+++ scripts/glue/gcc_insert.py
@@ -11,7 +11,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue import DynamicFrame
-from pyspark.sql.functions import to_timestamp, lit
+from pyspark.sql.functions import to_timestamp, lit, col
 
 
 def get_db_secret():
@@ -81,6 +81,35 @@ def upsert_spark_dataframe(partition, db_creds, database, schema, table):
     db_conn.close()
 
 
+def load_frame_from_jdbc(sparkObject, jdbc_url, user, password, engine, dbtable, schema):
+    if engine == "postgresql":
+        driver = "org.postgresql.Driver"
+    else:
+        return None
+
+    return (
+        sparkObject.read.format("jdbc")
+        .option("url", jdbc_url)
+        .option("driver", driver)
+        .option("dbtable", f"(SELECT * from {schema}.{dbtable}) as {dbtable}")
+        .option("user", user)
+        .option("password", password)
+        .load()
+    )
+
+def get_null_dataframe(frame, null_columns):
+    # Get dataframe with null values to log
+    conditions = [col(column_name).isNull() for column_name in null_columns]
+
+    # Combine conditions using OR operator
+    combined_condition = conditions[0]
+    for condition in conditions[1:]:
+        combined_condition = combined_condition | condition
+
+    # Filter the DataFrame based on the combined condition
+    null_frame = frame.filter(combined_condition)
+    return null_frame
+
 args = getResolvedOptions(
     sys.argv, ["JOB_NAME", "BUCKET_NAME", "OBJECT_NAME", "RUN_ID"]
 )
@@ -96,8 +125,13 @@ run_id = args.get("RUN_ID")
 
 # Get db credentials
 db_creds = get_db_secret()
+host = db_creds.get("host")
+user = db_creds.get("username")
+password = db_creds.get("password")
+port = db_creds.get("port")
 database = "historic_data"
 
+# Read csv
 read_frame = glueContext.create_dynamic_frame.from_options(
     format_options={
         "quoteChar": '"',
@@ -111,16 +145,19 @@ read_frame = glueContext.create_dynamic_frame.from_options(
     transformation_ctx="reading_csv",
 )
 
+
 if object_name == "departments":
     mappings = [
         ("col0", "string", "id", "bigint"),
         ("col1", "string", "department", "string"),
     ]
+    null_columns = ["department"]
 elif object_name == "jobs":
     mappings = [
         ("col0", "string", "id", "bigint"),
         ("col1", "string", "job", "string"),
     ]
+    null_columns = ["job"]
 elif object_name == "hired_employees":
     mappings = [
         ("col0", "string", "id", "bigint"),
@@ -129,6 +166,7 @@ elif object_name == "hired_employees":
         ("col3", "string", "department_id", "bigint"),
         ("col4", "string", "job_id", "bigint"),
     ]
+    null_columns = ["name", "datetime", "department_id", "job_id"]
 else:
     sys.exit()
 
@@ -141,16 +179,71 @@ proc_frame = ApplyMapping.apply(
 
 proc_frame = proc_frame.toDF().withColumn("run", lit(run_id))
 
+null_frame = get_null_dataframe(proc_frame, null_columns)
+null_frame.show(10)
 
-# Drop null fields for the hired employees table. It requires the IDs of jobs and departments as foreign keys
+# Drop null fields for important columns
+proc_frame = proc_frame.na.drop(subset=null_columns)
+
+# For hired_employees, check for job and departemnts not found in their corresponding tables
 if object_name == "hired_employees":
     proc_frame = proc_frame.withColumn(
         "datetime", to_timestamp("datetime", "yyyy-MM-dd'T'HH:mm:ssX")
     )
-    proc_frame = proc_frame.na.drop(
-        subset=["name", "job_id", "department_id", "datetime"]
+    proc_frame = proc_frame.na.drop(subset=null_columns)
+
+    # Read actual data from RDS to check for department and job IDs
+    jdbc_url = f"jdbc:postgresql://{host}:{port}/{database}"
+    df_actual_jobs = load_frame_from_jdbc(
+        sparkObject=spark,
+        jdbc_url=jdbc_url,
+        user=user,
+        password=password,
+        engine="postgresql",
+        dbtable="jobs",
+        schema="employees_data"
     )
-    # proc_frame = DynamicFrame.fromDF(df, glueContext, object_name)
+    df_actual_jobs = df_actual_jobs.withColumnRenamed("id", "job_id_actual").drop("run")
+
+    df_actual_departments = load_frame_from_jdbc(
+        sparkObject=spark,
+        jdbc_url=jdbc_url,
+        user=user,
+        password=password,
+        engine="postgresql",
+        dbtable="departments",
+        schema="employees_data"
+    )
+    df_actual_departments = df_actual_departments.withColumnRenamed("id", "department_id_actual").drop("run")
+
+    # No matching IDs dataframe
+    no_match_jobs_df = proc_frame.join(
+        df_actual_jobs, proc_frame.job_id == df_actual_jobs.job_id_actual, how="left_anti"
+    )
+    no_match_departments_df = proc_frame.join(
+        df_actual_departments,
+        proc_frame.department_id == df_actual_departments.department_id_actual,
+        how="left_anti",
+    )
+    no_match_df = no_match_jobs_df.union(no_match_departments_df).dropDuplicates()
+    no_match_df.show(10)
+
+    # Join with IDs to get only the hired employees with a valid department and job ID
+    proc_frame = (
+        proc_frame.join(
+            df_actual_jobs,
+            proc_frame.job_id == df_actual_jobs.job_id_actual,
+            how="inner",
+        )
+        .drop("job_id_actual").drop("job")
+    )
+    proc_frame = proc_frame.join(
+        df_actual_departments,
+        proc_frame.department_id == df_actual_departments.department_id_actual,
+        how="inner",
+    ).drop("department_id_actual").drop("department")
+
+    proc_frame.show(10)
 
 
 # Write Run dataframe
@@ -160,11 +253,6 @@ data = [
 run_dnf = DynamicFrame.fromDF(spark.createDataFrame(data=data), glueContext, "runs")
 
 # Write run data
-host = db_creds.get("host")
-user = db_creds.get("username")
-password = db_creds.get("password")
-port = db_creds.get("port")
-
 connection_options = {
     "url": f"jdbc:postgresql://{host}:{port}/{database}",
     "user": user,
