2d9d85a441dd14bcd4795894d8027bcb9639f401
maguzman5 <maguzman5@uc.cl>
2023-08-31 01:37:12 -0400

    lambda to trigger glue backup

--- terraform/lambda_pkg.tf
+++ terraform/lambda_pkg.tf
@@ -9,3 +9,9 @@ data "archive_file" "gcc_trigger_glue_zip" {
   source_file = "../scripts/lambda/gcc_trigger_glue/main.py"
   output_path = "../scripts/lambda/gcc_trigger_glue/main.zip"
 }
+
+data "archive_file" "gcc_trigger_backup_zip" {
+  type        = "zip"
+  source_file = "../scripts/lambda/gcc_trigger_backup/main.py"
+  output_path = "../scripts/lambda/gcc_trigger_backup/main.zip"
+}
